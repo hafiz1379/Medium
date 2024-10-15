4e2ebc95a53783c39e19ec172b50d4733eef0118
hafiz1379 <hafizrasa1379@gmail.com>
2024-10-15 20:09:32 +0430

    Update page layout to use a max width of 6xl instead of 7xl

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-6xl mx-auto">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
