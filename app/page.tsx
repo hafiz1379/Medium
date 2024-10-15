1a023355c69c1d8f9b23b8a3ebe8e923f550f462
hafiz1379 <hafizrasa1379@gmail.com>
2024-10-15 12:09:56 +0430

    Add Head component and set page title and favicon

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Medium Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Medium app</h1>
+    </div>
+  );
+}
