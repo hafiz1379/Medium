54adc9f286c94d71fb1592a008d6a52ac38fef83
hafiz1379 <hafizrasa1379@gmail.com>
2024-10-15 20:18:30 +0430

    Update styles in Header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -13,16 +13,16 @@ function Header() {
           />
         </Link>
         <div className="hidden md:inline-flex items-center space-x-5">
-          <h3>About</h3>
-          <h3>Contact</h3>
-          <h3 className="bg-green-600 text-white px-4 py-1 rounded-full">
+          <h3 className="cursor-pointer">About</h3>
+          <h3 className="cursor-pointer">Contact</h3>
+          <h3 className="bg-green-600 text-white px-4 py-1 rounded-full cursor-pointer">
             Follow
           </h3>
         </div>
       </div>
       <div className="flex items-center space-x-6 text-green-600">
-        <h3>Sing In</h3>
-        <h3 className="border px-4 py-1 rounded-full border-green-600">
+        <h3 className="cursor-pointer">Sing In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600 cursor-pointer hover:bg-green-600 hover:text-white transition duration-300">
           Get Started
         </h3>
       </div>
